8a917b3a9950a086a82b6517567bc10e4c2bead4
harrisonyeboah <harrisonyeboahcs@gmail.com>
2025-09-28 13:40:22 -0400

    Add files via upload

---
+++ minQueue.cpp
@@ -0,0 +1,183 @@
+#include <cassert>
+
+template <class T>
+minQueue<T>::minQueue() {
+    /*
+    This is the original contrstuctor
+    for my minQueue. This will be an empty min Queue 
+    heap with zero elements
+    */
+    numberOfElements = 0;
+    capacity = 100;
+    T* myArray = new T[capacity];
+}
+
+
+template <class T>
+minQueue<T>::minQueue(T* A, int n) {
+    /*
+    This constructor basically takes in an array
+    and the side of the array and initializes into the
+    min heap class.
+    */
+    numberOfElements = n;
+    capacity = n + 100;
+    myArray = new T[capacity];
+    for (int i = 0; i < numberOfElements; i++) {
+        myArray[i] = A[i];
+    } 
+}
+
+template <class T>
+minQueue<T>::~minQueue() {
+    /*
+    This is the destructor. This basically frees
+    allcated memory and makes eveything go back to zero and capacity to 0
+    Then what ever myArray points to will be a nullptr.
+    */
+    numberOfElements = 0;
+    capacity = 0;
+    delete[] myArray;
+    myArray = nullptr;
+}
+
+template <class T>
+minQueue<T>::minQueue(const minQueue<T>& other){
+    /*
+    This is my copy constructor. This basically aquires 
+    the capacity of the other and the numberOf Elements and 
+    copies others arrays into my created array
+    */
+    numberOfElements = other.numberOfElements;
+    capacity = other.capacity;
+    myArray = new T[capacity];
+    for (int i = 0; i < numberOfElements; i++) {
+        myArray[i] = other.myArray[i];
+    } 
+}
+
+template <class T>
+minQueue<T>& minQueue<T>::operator=(const minQueue<T>& other) {
+    /*
+    This is my equal operator overload which takes a minQueue and returns 
+    itself first it will delte everything in itself first. 
+    */
+
+
+    if (this == &other) {
+        return *this;
+    }
+    delete[] myArray;
+    numberOfElements = other.numberOfElements;
+    capacity = other.capacity;
+    myArray = new T[capacity];
+
+    for(int i = 0; i < numberOfElements; i++) {
+        myArray[i] = other.myArray[i];
+    }
+
+    return *this;
+
+}
+
+
+
+template <class T>
+void minQueue<T>::insert(T x) {
+    /*
+    This is the insert method basically. I will insert a node at the 
+    last element of the heap and trickel up ward until it 
+    statisfies the heap condition again. 
+    */
+    if (numberOfElements == capacity) {
+        capacity = capacity * 2;
+        T* newArray = new T[capacity];
+        for (int i = 0; i < numberOfElements; i++) {
+            newArray[i] = myArray[i];
+        }
+        delete[] myArray;
+        myArray = newArray;
+    }
+    myArray[numberOfElements] = x;
+    numberOfElements = numberOfElements + 1;
+    int addedNodedIndex = numberOfElements - 1;
+    while (addedNodedIndex > 0 && myArray[addedNodedIndex] < myArray[parent(addedNodedIndex)]) {
+        swap(addedNodedIndex, parent(addedNodedIndex));
+        addedNodedIndex = parent(addedNodedIndex);
+    }
+}
+
+
+
+template <class T>
+void minQueue<T>::minHeapify(int i) {
+    /*
+    This is the minHeapify method. The precondition of this method is overall 
+    the children are heaps and the post condition is index i will then be a heap. 
+    This runs recursively until index i is a heap. 
+    */
+    int l = left(i);
+    int r = right(i);
+    int smallest = i;
+
+    if (l < numberOfElements && myArray[l] < myArray[smallest]) {
+        smallest = l;
+    }
+    if (r < numberOfElements && myArray[r] < myArray[smallest]) {
+        smallest = r;
+    }
+
+    if (smallest != i) {
+        swap(smallest, i);
+        minHeapify(smallest);
+    }
+}
+
+void minQueue<T>::buildHeap() {
+    for (int i = parent(numberOfElements - 1) / 2; i >= 0; i--) {
+        minHeapify(i);
+    }
+}
+
+template <class T>
+void minQueue<T>::printHeap() {
+    /*
+    This is a method to see what is going on for debugging purposes
+    */
+    for (int i = 0; i < numberOfElements; i++) {
+        cout << "Element " << i << " is " << myArray[i] << ". " << endl;
+    }
+}
+
+
+void minQueue<T>::clear() {
+    /*
+    This is the clear function it is pretty simple 
+    i will delete the array and make it empty with zero elements
+    */
+    delete[] myArray;
+    capacity = 100;
+    numberOfElements = 0;
+    myArray = new T[capacity];
+}
+template <class T>
+int minQueue<T>::parent(int i) const {
+    assert(i > 0);
+    return (i - 1) / 2;
+}
+template <class T>
+int minQueue<T>::left(int i) const {
+    return (2 * i) + 1;
+}
+template <class T>
+int minQueue<T>::right(int i) const {
+    return (2 * i) + 2;
+}
+template <class T>
+void minQueue<T>::swap(int indexA, int indexB) {
+    // This simply just swaps/
+    T tempValue = myArray[indexB];
+    myArray[indexB] = myArray[indexA];
+    myArray[indexA] = tempValue;
+
+}
---
+++ minQueue.h
@@ -0,0 +1,53 @@
+//========================================================
+// CS 271
+// Fall 2025
+// minQueue.h
+// This file contains the minQueue class declaration.  
+// carefully.
+//========================================================
+
+#ifndef MINQUEUE_H
+#define MINQUEUE_H
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <cstdlib>
+
+template <class T> 
+class minQueue {
+    public:
+        minQueue();
+        minQueue(T* A, int n);
+        ~minQueue();
+        minQueue(const minQueue<T>& other);
+        minQueue<T>& operator=(const minQueue<T>& other);
+
+        void insert(T x);
+        T min() const; // Still need to do 
+        T extractMin(); // Still need to do 
+        void decreaseKey(int i, T k); // Still need to do
+        void minHeapify(int i);
+        void buildHeap(); 
+        void sort(T* A); // Still need to do 
+
+        void set(int i, T val); // Still need to do 
+        void allocate(int n); // Still need to do
+        std::string toString() const;
+        void printHeap(); // Still need to do 
+
+        void clear();
+        int parent(int i) const;
+        int left(int i) const;
+        int right(int i) const;
+        void swap (int indexA, indexB);
+
+    private:
+        int numberOfElements;
+        int capacity;
+        T* myArray;
+};
+
+#include "minQueue.cpp"
+
+#endif
