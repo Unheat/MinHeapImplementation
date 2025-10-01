b441274198005021ecb556825f4776170e8fef51
UnHeat <anbi2682006@gmail.com>
2025-09-30 23:17:36 -0400

    Fix MinQueue constructor

--- MinQueue.cpp
+++ MinQueue.cpp
@@ -1,35 +1,41 @@
 #include <cassert>
+#include "MinQueue.h"
+using namespace std;
 
 template <class T>
-minQueue<T>::minQueue() {
+MinQueue<T>::MinQueue() {
     /*
     This is the original contrstuctor
-    for my minQueue. This will be an empty min Queue 
+    for my MinQueue. This will be an empty min Queue 
     heap with zero elements
     */
     numberOfElements = 0;
-    capacity = 100;
-    T* myArray = new T[capacity];
+    capacity = 100; // it is ok but we have allocate function it would be better if we use it
+    myArray = new T[capacity]; //no pointer declare here
 }
 
 
 template <class T>
-minQueue<T>::minQueue(T* A, int n) {
+MinQueue<T>::MinQueue(T* A, int n) {
     /*
     This constructor basically takes in an array
     and the side of the array and initializes into the
     min heap class.
     */
+    if (n < 0) {
+        throw std::invalid_argument("n must be non-negative");
+    }
     numberOfElements = n;
     capacity = n + 100;
     myArray = new T[capacity];
     for (int i = 0; i < numberOfElements; i++) {
         myArray[i] = A[i];
     } 
+    buildHeap(); //call build heap here as we need a heap not a normal array
 }
 
 template <class T>
-minQueue<T>::~minQueue() {
+MinQueue<T>::~MinQueue() {
     /*
     This is the destructor. This basically frees
     allcated memory and makes eveything go back to zero and capacity to 0
@@ -42,7 +48,7 @@ minQueue<T>::~minQueue() {
 }
 
 template <class T>
-minQueue<T>::minQueue(const minQueue<T>& other){
+MinQueue<T>::MinQueue(const MinQueue<T>& other){
     /*
     This is my copy constructor. This basically aquires 
     the capacity of the other and the numberOf Elements and 
@@ -57,9 +63,9 @@ minQueue<T>::minQueue(const minQueue<T>& other){
 }
 
 template <class T>
-minQueue<T>& minQueue<T>::operator=(const minQueue<T>& other) {
+MinQueue<T>& MinQueue<T>::operator=(const MinQueue<T>& other) {
     /*
-    This is my equal operator overload which takes a minQueue and returns 
+    This is my equal operator overload which takes a MinQueue and returns 
     itself first it will delte everything in itself first. 
     */
 
@@ -83,21 +89,13 @@ minQueue<T>& minQueue<T>::operator=(const minQueue<T>& other) {
 
 
 template <class T>
-void minQueue<T>::insert(T x) {
+void MinQueue<T>::insert(T x) {
     /*
     This is the insert method basically. I will insert a node at the 
     last element of the heap and trickel up ward until it 
     statisfies the heap condition again. 
     */
-    if (numberOfElements == capacity) {
-        capacity = capacity * 2;
-        T* newArray = new T[capacity];
-        for (int i = 0; i < numberOfElements; i++) {
-            newArray[i] = myArray[i];
-        }
-        delete[] myArray;
-        myArray = newArray;
-    }
+    allocate(numberOfElements+1) //use allocate function
     myArray[numberOfElements] = x;
     numberOfElements = numberOfElements + 1;
     int addedNodedIndex = numberOfElements - 1;
@@ -110,7 +108,7 @@ void minQueue<T>::insert(T x) {
 
 
 template <class T>
-void minQueue<T>::minHeapify(int i) {
+void MinQueue<T>::minHeapify(int i) {
     /*
     This is the minHeapify method. The precondition of this method is overall 
     the children are heaps and the post condition is index i will then be a heap. 
@@ -133,14 +131,15 @@ void minQueue<T>::minHeapify(int i) {
     }
 }
 
-void minQueue<T>::buildHeap() {
+template <class T> 
+void MinQueue<T>::buildHeap() {
     for (int i = parent(numberOfElements - 1) / 2; i >= 0; i--) {
         minHeapify(i);
     }
 }
 
 template <class T>
-void minQueue<T>::printHeap() {
+void MinQueue<T>::printHeap() {
     /*
     This is a method to see what is going on for debugging purposes
     */
@@ -149,8 +148,8 @@ void minQueue<T>::printHeap() {
     }
 }
 
-
-void minQueue<T>::clear() {
+template <class T> 
+void MinQueue<T>::clear() {
     /*
     This is the clear function it is pretty simple 
     i will delete the array and make it empty with zero elements
@@ -161,23 +160,22 @@ void minQueue<T>::clear() {
     myArray = new T[capacity];
 }
 template <class T>
-int minQueue<T>::parent(int i) const {
+int MinQueue<T>::parent(int i) const {
     assert(i > 0);
     return (i - 1) / 2;
 }
 template <class T>
-int minQueue<T>::left(int i) const {
+int MinQueue<T>::left(int i) const {
     return (2 * i) + 1;
 }
 template <class T>
-int minQueue<T>::right(int i) const {
+int MinQueue<T>::right(int i) const {
     return (2 * i) + 2;
 }
 template <class T>
-void minQueue<T>::swap(int indexA, int indexB) {
+void MinQueue<T>::swap(int indexA, int indexB) {
     // This simply just swaps/
     T tempValue = myArray[indexB];
     myArray[indexB] = myArray[indexA];
     myArray[indexA] = tempValue;
-
 }
--- MinQueue.h
+++ MinQueue.h
@@ -1,13 +1,13 @@
 //========================================================
 // CS 271
 // Fall 2025
-// minQueue.h
-// This file contains the minQueue class declaration.  
+// MinQueue.h
+// This file contains the MinQueue class declaration.  
 // carefully.
 //========================================================
 
-#ifndef MINQUEUE_H
-#define MINQUEUE_H
+#ifndef MinQueue_H
+#define MinQueue_H
 
 #include <iostream>
 #include <string>
@@ -15,44 +15,40 @@
 #include <cstdlib>
 
 template <class T> 
-class minQueue {
+class MinQueue{
     public:
-        minQueue();
-        minQueue(T* A, int n);
-        ~minQueue();
-        minQueue(const minQueue<T>& other);
-        minQueue<T>& operator=(const minQueue<T>& other);
+        MinQueue();
+        MinQueue(T* A, int n);
+        ~MinQueue();
+        MinQueue(const MinQueue<T>& other);
+        MinQueue<T>& operator=(const MinQueue<T>& other);
 
         void insert(T x);
         T min() const; // Still need to do 
-        T extractMin(); // Still need to do 
-        void decreaseKey(int i, T k); // Still need to do
-        void minHeapify(int i);
-        void buildHeap(); 
+        T extract_min(); // Still need to do 
+        void decrease(int i, T k); // Still need to do
+        void min_heapify(int i);
+        void build_heap()(); 
         void sort(T* A); // Still need to do 
 
         void set(int i, T val); // Still need to do 
         void allocate(int n); // Still need to do
-        std::string toString() const;
+        std::string to_string() const;
         void printHeap(); // Still need to do 
 
         void clear();
-        int parent(int i) const;
-        int left(int i) const;
-        int right(int i) const;
-        void swap (int indexA, indexB);
 
     private:
         int numberOfElements;
         int capacity;
         T* myArray;
-        
+
         int parent(int i) const;
         int left(int i) const;
         int right(int i) const;
-        void swap (int indexA, indexB);
+        void swap(int indexA, int indexB);
 };
 
-#include "minQueue.cpp"
+#include "MinQueue.cpp"
 
 #endif
